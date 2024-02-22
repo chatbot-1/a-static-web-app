36b4f4c10a9616f4f941ef288c6c58a85f6b6281
chatbot-3 <sensationalatul10@gmail.com>
2024-02-22 14:27:18 +0530

    Updated the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <section>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p>How are you doing?</p>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
